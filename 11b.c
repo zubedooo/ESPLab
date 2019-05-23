6e006e466cede445dcb3b3450805b65ae0bc34a7
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-23 23:43:57 +0530

    Update 11b.c

--- 11b.c
+++ 11b.c
@@ -6,30 +6,23 @@
 
 void EINT1Callback() {
 DrvGPIO_Open(E_GPC, 12, E_IO_OUTPUT);
-    DrvGPIO_Open(E_GPC, 13, E_IO_OUTPUT);
-    DrvGPIO_Open(E_GPC, 14, E_IO_OUTPUT);
-    DrvGPIO_Open(E_GPC, 15, E_IO_OUTPUT);
-    
-    DrvGPIO_SetPortBits(E_GPC,0x7000);
-        DrvSYS_Delay(100000);
-        DrvGPIO_SetPortBits(E_GPC,0xb000);
-        DrvSYS_Delay(100000);
-        DrvGPIO_SetPortBits(E_GPC,0xd000);
-        DrvSYS_Delay(100000);
-        DrvGPIO_SetPortBits(E_GPC,0xe000);
-        DrvSYS_Delay(100000);
-        
+DrvGPIO_Open(E_GPC, 13, E_IO_OUTPUT);
+DrvGPIO_Open(E_GPC, 14, E_IO_OUTPUT);
+DrvGPIO_Open(E_GPC, 15, E_IO_OUTPUT);
+DrvGPIO_SetPortBits(E_GPC,0x7000);
+DrvSYS_Delay(100000);
+DrvGPIO_SetPortBits(E_GPC,0xb000);
+DrvSYS_Delay(100000);
+DrvGPIO_SetPortBits(E_GPC,0xd000);
+DrvSYS_Delay(100000);
+DrvGPIO_SetPortBits(E_GPC,0xe000);
+DrvSYS_Delay(100000);       
 }
-
 int main (void)
-{                       
-    UNLOCKREG(); 
-    DrvSYS_Open(48000000); 
-    LOCKREG();
-
+{UNLOCKREG(); 
+DrvSYS_Open(48000000); 
+LOCKREG();
 DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE,E_MODE_EDGE,EINT1Callback);
-
-    while(1) 
-          
-    }
+while(1) 
+{}
 }
