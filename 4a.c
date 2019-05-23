78c80dee498a6e830682ec16c907aff8277b44b7
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-23 22:56:49 +0530

    Update 4a.c

--- 4a.c
+++ 4a.c
@@ -5,15 +5,13 @@
 #include "DrvADC.h"
 #include "LCD_Driver.h" 
  int32_t main (void)
-{    
-	UNLOCKREG();
-	LOCKREG();
-	InitPWM();
-	DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x80,INTERNAL_HCLK , 1); 	
-	while(1)
-   {
-    DrvADC_StartConvert();   // start A/D conversion
-    while(DrvADC_IsConversionDone()==FALSE);
-     PWMA->CMR0=ADC->ADDR[7].RSLT<<4;	 
-    }
+{UNLOCKREG();
+LOCKREG();
+InitPWM();
+DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x80,INTERNAL_HCLK , 1); 	
+while(1)
+{DrvADC_StartConvert();   // start A/D conversion
+while(DrvADC_IsConversionDone()==FALSE);
+PWMA->CMR0=ADC->ADDR[7].RSLT<<4;	 
+}
 }	
