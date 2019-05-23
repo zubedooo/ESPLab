4e6dde30f15c35e0d278c5d90f2bac71c09e03b0
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-23 23:44:57 +0530

    Update 12a.c

--- 12a.c
+++ 12a.c
@@ -4,35 +4,23 @@
 #include "Driver\DrvGPIO.h"
 #include "NUC1xx-LB_002\LCD_Driver.h"
 
-
 int main (void)
-{
-int32_t number;
-    char TEXT0[16]="SmplKeypad";
-
+{int32_t number;
+char TEXT0[16]="SmplKeypad";
 char TEXT1[16];
-    UNLOCKREG();               
-  DrvSYS_Open(48000000);
-                          
-    LOCKREG();                    
-
-
-    Initial_panel(); 
-    clr_all_panel();    
-        
-    print_lcd(0,TEXT0);
-    
-    while (1)                   
-    {
-        number=DrvGPIO_GetPortBits(E_GPA);
-            
-         sprintf(TEXT1,"%x",number);           
-        print_lcd(1,TEXT1); 
-        if(number==0xfffe)
-            print_lcd(2,"A0 grounded");
-        else if(number==0xffff)
-            print_lcd(2,"not grounded");     // grounded ?
-    }
-
+UNLOCKREG();               
+DrvSYS_Open(48000000);
+LOCKREG();                    
+Initial_panel(); 
+clr_all_panel();    
+print_lcd(0,TEXT0);
+while (1)                   
+{number=DrvGPIO_GetPortBits(E_GPA);
+sprintf(TEXT1,"%x",number);           
+print_lcd(1,TEXT1); 
+if(number==0xfffe)
+print_lcd(2,"A0 grounded");
+else if(number==0xffff)
+print_lcd(2,"not grounded");     // grounded ?
 }
-
+}
