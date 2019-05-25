4f9698f12e1851ce9c2c52081196afa8b00011be
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-25 12:56:34 +0530

    Update 7b.c

--- 7b.c
+++ 7b.c
@@ -1,25 +1,26 @@
-#include <stdio.h>                                                                                                             
+#include <stdio.h>
 #include "NUC1xx.h"
 #include "DrvSYS.h"
 #include "DrvGPIO.h"
 #include "scankey.h"
 #include "NUC1xx-LB_002\LCD_Driver.h"
+
 int32_t main (void)
 {int8_t number;
 char TEXT0[16]="Smpl_LCD_Keypad";
-char TEXT1[16]="Keypad:        ";
+char TEXT1[16]="Keypad: ";
 UNLOCKREG();
-DrvSYS_Open(48000000); // set MCU to run at 48MHz
+DrvSYS_Open(48000000);
 LOCKREG();
-Initial_panel(); 
+Initial_panel();
 clr_all_panel();
-OpenKeyPad();        // initialize 3x3 keypad
-print_lcd(0,TEXT0); // print title
+OpenKeyPad();
+print_lcd(0,TEXT0);
 while(1)
-{number = Scankey();           // scan keypad to input
-if(number)  
-sprintf(TEXT1+8,"%d",number); // print scankey input to string              
-print_lcd(1, TEXT1);          // display string on LCD
-DrvSYS_Delay(5000);           // delay                                                                      
+{
+number = Scankey();
+sprintf(TEXT1+8,"%d",number);
+print_lcd(1, TEXT1);
+DrvSYS_Delay(5000);
 }
 }
