2a09409bc39ab08b5c13ee28503f9a11968f4eff
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-25 12:43:02 +0530

    Update 3b.c

--- 3b.c
+++ 3b.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 #include "NUC1xx.h"
 #include "Driver\DrvGPIO.h"
-#include "Driver\DrvUART.h"
 #include "Driver\DrvSYS.h"
 #include "LCD_Driver.h"
-
-// External Interrupt Handler (INT button to trigger GPB15)
-void EINT1Callback(void) 
-{DrvGPIO_ClrBit(E_GPB,11); // GPB11 = 0 to turn on Buzzer
-//DrvSYS_Delay(100000);	    // Delay 
-DrvGPIO_SetBit(E_GPB,11); // GPB11 = 1 to turn off Buzzer	
-//DrvSYS_Delay(100000);	    // Delay 	
-Initial_panel(); 
-clr_all_panel();
-print_lcd(0,"Your Name");
-DrvSYS_Delay(10000000);	    
-clr_all_panel();
+void EINT1Callback()
+{
+print_lcd(1, "Interrupt");
 }
-int main (void)
-{UNLOCKREG();
-//DrvSYS_SetOscCtrl(E_SYS_XTL12M, 1); // external 12MHz Crystal
-DrvSYS_Delay(5000);                 // delay for stable clock
-//DrvSYS_SelectHCLKSource(0);         // clock source = 12MHz Crystal
+int main()
+{
+char TEXT[16];
+UNLOCKREG();
+DrvSYS_Open(48000000);
 LOCKREG();
-DrvGPIO_Open(E_GPB, 11, E_IO_OUTPUT); // initial GPIO pin GPB11 for controlling Buzzer
-// External Interrupt 
-DrvGPIO_Open(E_GPB, 15, E_IO_INPUT);                             // configure external interrupt pin GPB15
-DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE, E_MODE_EDGE, EINT1Callback); // configure external interrupt
-while(1)
-{}
+Initial_panel();
+clr_all_panel();
+print_lcd(0, "Smpl_GPIO_Intr");
+DrvGPIO_Open(E_GPB, 15, E_IO_INPUT);
+DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE, E_MODE_EDGE, EINT1Callback);
+while(1){}
 }
