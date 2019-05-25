4a2a0ceeb7b42b956e96d29f12b03206900d21d7
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-25 12:16:06 +0530

    Update 1b.c

--- 1b.c
+++ 1b.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 #include "NUC1xx.h"
-#include "Driver\DrvSYS.h"
 #include "Driver\DrvGPIO.h"
-#include "Driver\DrvADC.h"
-void EINT1Callback() 
+#include "Driver\DrvUART.h"
+#include "Driver\DrvSYS.h"
+void EINT1Callback()
+{DrvGPIO_ClrBit(E_GPC, 12);
+DrvSYS_Delay(100000);
+DrvGPIO_SetBit(E_GPC, 12);
+DrvSYS_Delay(100000);
+DrvGPIO_ClrBit(E_GPC, 13);
+DrvSYS_Delay(100000);
+DrvGPIO_SetBit(E_GPC, 13);
+DrvSYS_Delay(100000);
+DrvGPIO_ClrBit(E_GPC, 14);
+DrvSYS_Delay(100000);
+DrvGPIO_SetBit(E_GPC, 14);
+DrvSYS_Delay(100000);
+DrvGPIO_ClrBit(E_GPC, 15);
+DrvSYS_Delay(100000);
+DrvGPIO_SetBit(E_GPC, 15);
+DrvSYS_Delay(100000);
+}
+void Init_LED()
 {DrvGPIO_Open(E_GPC, 12, E_IO_OUTPUT);
 DrvGPIO_Open(E_GPC, 13, E_IO_OUTPUT);
 DrvGPIO_Open(E_GPC, 14, E_IO_OUTPUT);
 DrvGPIO_Open(E_GPC, 15, E_IO_OUTPUT);
-DrvGPIO_SetPortBits(E_GPC,0x7000);
-DrvSYS_Delay(100000);
-DrvGPIO_SetPortBits(E_GPC,0xb000);
-DrvSYS_Delay(100000);
-DrvGPIO_SetPortBits(E_GPC,0xd000);
-DrvSYS_Delay(100000);
-DrvGPIO_SetPortBits(E_GPC,0xe000);
-DrvSYS_Delay(100000);
+DrvGPIO_SetBit(E_GPC, 12);
+DrvGPIO_SetBit(E_GPC, 13);
+DrvGPIO_SetBit(E_GPC, 14);
+DrvGPIO_SetBit(E_GPC, 15);
 }
 int main (void)
-{UNLOCKREG(); 
-DrvSYS_Open(48000000); 
+{
+UNLOCKREG();
+DrvSYS_Open(48000000);
 LOCKREG();
-DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE,E_MODE_EDGE,EINT1Callback);
-while(1)    
+Init_LED();
+DrvGPIO_Open(E_GPB, 15, E_IO_INPUT);
+DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE, E_MODE_EDGE, EINT1Callback);
+while (1)
 {}
 }
