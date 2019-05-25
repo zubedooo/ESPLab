c0ff7d53b26f4103aa94bec7cf365f0088aaa1bc
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-25 12:41:41 +0530

    Update 2a.c

--- 2a.c
+++ 2a.c
@@ -31,20 +31,18 @@ int32_t main (void)
 { uint16_t value;
 char TEXT[16];
 UNLOCKREG();
-DrvSYS_Open(48000000);
-//SYSCLK->PWRCON.XTL12M_EN = 1; //Enable 12Mhz and set HCLK->12Mhz
-//SYSCLK->CLKSEL0.HCLK_S = 0;
+SYSCLK->PWRCON.XTL12M_EN = 1; //Enable 12Mhz and set HCLK->12Mhz
+SYSCLK->CLKSEL0.HCLK_S = 0;
 LOCKREG();
 Initial_panel(); // initialize LCD pannel
 clr_all_panel(); // clear LCD panel
-print_lcd(0,"variable reistor");
-DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x04, INTERNAL_HCLK ,
-1);
+print_lcd(0,"variable resistor");
+DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x80,INTERNAL_HCLK , 1);
 while(1)
 {
 DrvADC_StartConvert(); // start A/D conversion
 while(DrvADC_IsConversionDone()==FALSE);
-value = ADC->ADDR[ 2 ].RSLT;// & 0xFFF;
+value = ADC->ADDR[7].RSLT & 0xFFF;
 sprintf(TEXT,"Value: %d",value); // convert ADC0 value into text
 print_lcd(1, TEXT); // output TEXT to LCD
 seg_display(value);
