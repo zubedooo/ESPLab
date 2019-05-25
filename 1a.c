4304b5cb237605919a4b063fbb9b7a0c194a550f
zubedooo <34791155+zubedooo@users.noreply.github.com>
2019-05-25 12:14:57 +0530

    Update 1a.c

--- 1a.c
+++ 1a.c
@@ -1,28 +1,28 @@
-#include <stdio.h>																											 
+#include <stdio.h>
 #include "NUC1xx.h"
 #include "Driver\DrvSYS.h"
-#include "DrvPWM.h"
+#include "NUC1xx-LB_002\Seven_Segment.h"
 #include "DrvADC.h"
-#include "LCD_Driver.h"
-#include "Driver\DrvGPIO.h"
+#include "NUC1xx-LB_002\LCD_Driver.h"
 
 int32_t main (void)
-{char value[15];  
-float volt = 0;
+{uint16_t value;
+char TEXT[16];
 UNLOCKREG();
-DrvSYS_Open(12000000);
+SYSCLK->PWRCON.XTL12M_EN = 1; //Enable 12Mhz and set HCLK->12Mhz
+SYSCLK->CLKSEL0.HCLK_S = 0;
 LOCKREG();
-//InitPWM(6);//inbuilt
-Initial_panel();  // initialize LCD pannel
-clr_all_panel();  // clear LCD panel 
+Initial_panel(); // initialize LCD pannel
+clr_all_panel(); // clear LCD panel
 print_lcd(0,"variable resistor");
-DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x40,INTERNAL_HCLK , 1); 	//0x40 FOR CHANNEL 6
+DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x40,INTERNAL_HCLK , 1);
 while(1)
-{DrvADC_StartConvert();   // start A/D conversion
+{
+DrvADC_StartConvert(); // start A/D conversion
 while(DrvADC_IsConversionDone()==FALSE);
-PWMA->CMR0=ADC->ADDR[6].RSLT<<4;
-volt = ((float)ADC->ADDR[6].RSLT/4095.0  )*5;
-sprintf(value,"%fV",volt);  //ADC CHANNEL 6
-print_lcd(1, value);
+value = ADC->ADDR[6].RSLT & 0xFFF;
+value=value*3.3/4095;
+sprintf(TEXT,"Value: %d",value); // convert ADC0 value into text
+print_lcd(1, TEXT); // output TEXT to LCD
 }
 }
