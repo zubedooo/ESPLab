322516f148f068488db16210c7fb45d5cb73f7ac
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-05-28 14:49:49 +0530

    Update 11a.c

--- 11a.c
+++ 11a.c
@@ -2,19 +2,14 @@
 #include "NUC1xx.h"
 #include "Driver\DrvSYS.h"
 #include "DrvPWM.h"
-#include "DrvADC.h"
-#include "LCD_Driver.h"
+#include "DrvSYS.h"
 
-int32_t main (void)
+int main ()
 {int16_t x=0xffff;
 UNLOCKREG();
-SYSCLK->PWRCON.XTL12M_EN = 1;
-SYSCLK->CLKSEL0.HCLK_S = 0;
+DrvSYS_Open(48000000);
 LOCKREG();
 InitPWM();
-Initial_panel(); // initialize LCD pannel
-clr_all_panel(); // clear LCD panel
-print_lcd(0,"variable reistor");
 while(1)
 {PWMA->CMR0=x;
 x = x - 1;
